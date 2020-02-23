ddfa3c80591e9f9d4b0f8abe556e56ff40668b8b
Glitch (ellipsis-dev) <none>
2020-02-23 18:11:23 +0000

    [dev] Moved some codes to module

--- modules/portalknights.js
+++ modules/portalknights.js
@@ -1,74 +1,73 @@
 'use strict'
-
 /*
 |------------------------------------------------------------------------------------
 | This module is exporting function as a class to display game contents in RichEmbed
 |------------------------------------------------------------------------------------
 */
+const Discord         = require('discord.js');
+const cmd             = require("../commands/portalknights.json");
+const embedData       = require("../embed.json");
+const embedFieldData  = require("../embed_fields/portalknights.json");
 
-const DISCORD = require('discord.js');
-
-module.exports.CNT_PortalKnights = function (receivedCommand, embed) {
-
-  // Declare a let variable to store all embed fields data
-  let embedFields = embed.fields;
+module.exports.run = async (message, secondaryCmd, args) => {
   
-  // Set the embed fields data from 0 - 5
-  embed.fields = embedFields.slice(0, 5);
+    this.secondaryCmd = secondaryCmd;
+    this.args = args;
 
-  // Create an embed object with embed data
-  let embedDisplay = new DISCORD.RichEmbed(embed)                  
+    let embed = embedData.portalknights[this.secondaryCmd][this.args];
+    let embedFields = embedFieldData[this.secondaryCmd][this.args];          
 
-  receivedCommand.channel.send({embed : embedDisplay}).then(embedMessage => {
-    embedMessage.react('➡️');
-    embedMessage.react('⬅️');
+    embed.fields = embedFields.slice(0, 5); // Set the embed fields data from 0 - 5
+    let embedDisplay = new Discord.RichEmbed(embed); // Create an embed object with embed data               
 
-    const filter = (reaction , user) => {
-      return ['⬅️', '➡️'].includes(reaction.emoji.name) && user.id === receivedCommand.author.id;
-    };
+    message.channel.send({embed : embedDisplay}).then(async embedMessage => {
+      await embedMessage.react('⬅️');
+      await embedMessage.react('➡️');
 
-    const collector = embedMessage.createReactionCollector(filter, { time: 60000 * 5}); // 1 min
+      const filter = (reaction , user) => {
+        return ['⬅️', '➡️'].includes(reaction.emoji.name) && user.id === message.author.id;
+      };
 
-    collector.on('collect', reaction => {
+      const collector = embedMessage.createReactionCollector(filter, { time: 60000 * 5}); // 1 min
 
-      // Next page button                        
-      if (reaction.emoji.name == '➡️') {
-        
-        let indexList = embedFields.indexOf(embed.fields[4]);
-        embed.fields = embedFields.slice(indexList+1,indexList+6);
+      collector.on('collect', reaction => {
 
-        if (indexList == -1) {
-
-          embed.fields = embedFields.slice(0,5);
-          let embedDisplay = new DISCORD.RichEmbed(embed);
-          embedMessage.edit(embedDisplay)
-
-        } 
-
-        let embedDisplay = new DISCORD.RichEmbed(embed);
-        embedMessage.edit(embedDisplay)
-
-
-      } else {
+        // Next page button                        
+        if (reaction.emoji.name == '➡️') {
 
           let indexList = embedFields.indexOf(embed.fields[4]);
-          embed.fields = embedFields.slice(indexList-9,indexList-4);
+          embed.fields = embedFields.slice(indexList+1,indexList+6);
 
-          if (indexList <= 4) {
+          if (indexList == -1) {
 
             embed.fields = embedFields.slice(0,5);
-            let embedDisplay = new DISCORD.RichEmbed(embed);
+            let embedDisplay = new Discord.RichEmbed(embed);
             embedMessage.edit(embedDisplay)
 
           } 
 
-          let embedDisplay = new DISCORD.RichEmbed(embed);
+          let embedDisplay = new Discord.RichEmbed(embed);
           embedMessage.edit(embedDisplay)
 
-      }
-    })                
-  })
-  .catch(err => console.error(err));
 
+        } else {
 
+            let indexList = embedFields.indexOf(embed.fields[4]);
+            embed.fields = embedFields.slice(indexList-9,indexList-4);
+
+            if (indexList <= 4) {
+
+              embed.fields = embedFields.slice(0,5);
+              let embedDisplay = new Discord.RichEmbed(embed);
+              embedMessage.edit(embedDisplay)
+
+            } 
+
+            let embedDisplay = new Discord.RichEmbed(embed);
+            embedMessage.edit(embedDisplay)
+
+        }
+      })                
+    })
+    .catch(err => console.error(err));
 }
